016c4d10afa0ce74b7105fed6a17ed7cd120f328
danivent23122020 <dani.vent@orange.fr>
2022-09-17 10:31:38 +0200

    commit

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// modal
-const myModal = document.getElementById("myModal");
-const myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", () => {
-    myInput.focus();
-});
